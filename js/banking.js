07503c0835cfa97bb73d3d97161353c468c1b863
progmamun <mamun333089@gmail.com>
2022-02-10 12:27:46 +0600

    update

--- js/banking.js
+++ js/banking.js
@@ -5,11 +5,24 @@ document
   .addEventListener('click', function () {
     // get the amount deposited
     const depositInput = document.getElementById('deposit-input');
-    const depositAmount = depositInput.value;
 
+    const newDepositAmountText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositAmountText);
+
+    // update deposit total
     const depositTotal = document.getElementById('deposit-total');
-    depositTotal.innerText = depositAmount;
 
-    // clear the deposit input field
-    depositInput.value = '';
+    const previousDepositText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositText);
+
+    const newDepositTotal =
+      parseFloat(previousDepositAmount) + parseFloat(newDepositAmount);
+
+    depositTotal.innerText = newDepositTotal;
+
+    // Update account balanace
+    const balanceTotal = balanceTotal.innerText;
+    const previousBalanceTotal =
+      // clear the deposit input field
+      (depositInput.value = '');
   });
