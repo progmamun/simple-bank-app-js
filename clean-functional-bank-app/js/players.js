f40a5031aa3abd696b2083e1c44413a715327f31
progmamun <mamun333089@gmail.com>
2022-02-18 19:36:30 +0600

    query-sector

--- clean-functional-bank-app/js/players.js
+++ clean-functional-bank-app/js/players.js
@@ -1,13 +1,33 @@
-const players = document.getElementsByClassName('player');
-
-for (const player of players) {
-  console.log(player);
+function setPlayerStyle(player) {
   player.style.border = '1px solid red';
   player.style.borderRadius = '15px';
   player.style.margin = '10px';
   player.style.padding = '10px';
 }
 
-function addPlayer() {
-  const playerContainer = document.getElementById('player');
+const players = document.getElementsByClassName('player');
+
+for (const player of players) {
+  setPlayerStyle(player);
 }
+
+function addPlayer() {
+  const playersContainer = document.getElementById('players');
+  const player = document.createElement('div');
+  player.classList.add('player');
+  player.innerHTML = `
+  <h4 class="player-name">New Player</h4>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, alias repellat fuga neque iure ipsa quaerat et ut praesentium repudiandae, ullam omnis debitis quasi vero deserunt aliquam, ab perspiciatis est!</p>
+  `;
+  setPlayerStyle(player);
+  playersContainer.appendChild(player);
+}
+
+document.getElementById('players').addEventListener('click', function (event) {
+  // e.preventDefault();
+  if (event.target.tagName.toLowerCase() == 'div') {
+    event.target.style.backgroundColor = 'yellow';
+  } else {
+    event.target.parentNode.style.backgroundColor = 'yellow';
+  }
+});
