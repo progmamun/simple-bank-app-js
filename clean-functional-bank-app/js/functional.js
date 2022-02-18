29264d162925bb060c44bba2cb57ad41fed5a07b
progmamun <mamun333089@gmail.com>
2022-02-18 18:43:10 +0600

    update-function

--- clean-functional-bank-app/js/functional.js
+++ clean-functional-bank-app/js/functional.js
@@ -5,39 +5,56 @@ function getInputValue(fieldId) {
   inputField.value = '';
   return value;
 }
+function getInnerTextValue(fieldId) {
+  const fieldTag = document.getElementById(fieldId);
+  const fieldValueInText = fieldTag.innerText;
+  const value = parseFloat(fieldValueInText);
+  return value;
+}
 function updateTotal(fieldId, amount) {
-  const totalTag = document.getElementById(fieldId);
-  const previousTotalInText = totalTag.innerText;
-  const previousTotal = parseFloat(previousTotalInText);
+  // const totalTag = document.getElementById(fieldId);
+  // const previousTotalInText = totalTag.innerText;
+  // const previousTotal = parseFloat(previousTotalInText);
+  const previousTotal = getInnerTextValue(fieldId);
   const newTotal = previousTotal + amount;
-  totalTag.innerText = newTotal;
+  // totalTag.innerText = newTotal;
+  document.getElementById(fieldId).innerText = newTotal;
+  return newTotal;
 }
 
-function updateBalance(amount, isAdd) {
-  const balanceTag = document.getElementById('balance-total');
-  const balanceInText = balanceTag.innerText;
-  const previousBalance = parseFloat(balanceInText);
+function updateBalance(amount, isAdding) {
+  // const balanceTag = document.getElementById('balance-total');
+  // const balanceInText = balanceTag.innerText;
+  // const previousBalance = parseFloat(balanceInText);
+  const previousBalance = getInnerTextValue('balance-total');
   let newBalance;
   if (isAdding == true) {
     newBalance = previousBalance + amount;
   } else {
     newBalance = previousBalance - amount;
   }
-  balanceTag.innerText = newBalance;
+  // balanceTag.innerText = newBalance;
+  document.getElementById('balance-total').innerText = newBalance;
 }
 
 document
   .getElementById('deposit-button')
   .addEventListener('click', function () {
     const amount = getInputValue('deposit-input');
-    updateTotal('deposit-total', amount);
+    if (amount > 0) {
+      updateTotal('deposit-total', amount);
+      updateBalance(amount, true);
+    }
   });
 
 // handle withdraw
 document
   .getElementById('withdraw-button')
   .addEventListener('click', function () {
-    const amount = getInputValue();
-    updateTotal('withdraw-total');
-    updateBalance(amount, false);
+    const amount = getInputValue('withdraw-input');
+    const balance = getInnerTextValue('balance-total');
+    if (amount > 0 && amount <= balance) {
+      updateTotal('withdraw-total', amount);
+      updateBalance(amount, false);
+    }
   });
