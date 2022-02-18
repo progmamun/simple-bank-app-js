7c3ea14a2c3b8d5f7d42a7f4ab916520860c945a
progmamun <mamun333089@gmail.com>
2022-02-18 14:58:54 +0600

    functional

---
+++ clean-functional-bank-app/js/functional.js
@@ -0,0 +1,13 @@
+function getInputValue(fieldId) {
+  const inputField = document.getElementById(fieldId);
+  const valueInText = inputField.value;
+  const value = parseFloat(valueInText);
+  inputField.value = '';
+  return value;
+}
+
+document
+  .getElementById('deposit-button')
+  .addEventListener('click', function () {
+    const amount = getInputValue('deposit-input');
+  });
